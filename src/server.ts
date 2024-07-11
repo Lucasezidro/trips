bd6f2f1fcbb679c5b05fe3a87258167fe813ee44
lucasezidro <lucasezidro7@gmail.com>
2024-07-11 08:54:05 -0300

    create last routes

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,10 @@ import { createActivity } from './routes/create-activity'
 import { getActivities } from './routes/get-activities'
 import { createLink } from './routes/create-link'
 import { getLinks } from './routes/get-links'
+import { getParticipants } from './routes/get-participants'
+import { createInvite } from './routes/create-invite'
+import { updateTrip } from './routes/update-trip'
+import { getParticipant } from './routes/get-participant'
 
 const app = fastify()
 
@@ -28,5 +32,9 @@ app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
 app.register(getLinks)
+app.register(getParticipants)
+app.register(getParticipant)
+app.register(createInvite)
+app.register(updateTrip)
 
 app.listen({ port: 3333 }).then(() => console.log('HTTP server running!'))
